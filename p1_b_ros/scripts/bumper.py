98b47305e65a97b083df1b29945f0f3f24dbc948
guidiamond <gu1k1ll3rbr@hotmail.com>
2019-04-19 00:51:08 -0300

    mudanca bumper

--- p1_b_ros/scripts/bumper.py
+++ p1_b_ros/scripts/bumper.py
@@ -3,3 +3,17 @@ def bumpeiras(dado):
     global bump
     bump = dado.data
     print('Bump:',bump)
+# Loop de execucao, apos verificar o scan
+if bump:
+    if bump == 1:
+        vel = Twist(Vector3(-1.5, 0, 0), Vector3(0, 0, -1))
+    elif bump == 2:
+        vel = Twist(Vector3(-1.5, 0, 0), Vector3(0, 0, 1))
+    elif bump == 3:
+        vel = Twist(Vector3(0.4, 0, 0), Vector3(0, 0, pi/4))
+    else:
+        vel = Twist(Vector3(0.4, 0, 0), Vector3(0, 0, -pi/4))
+    bump = 0
+    velocidade_saida.publish(vel)
+    rospy.sleep(3)
+    continue
